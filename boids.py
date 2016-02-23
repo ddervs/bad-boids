a23e2fa050e520c9ca6ae3f509e666f68e640cf0
Danial Dervovic <dannydervovic@hotmail.com>
2016-02-23 20:00:15 +0000

    Config file for sim

--- boids.py
+++ boids.py
@@ -1,41 +1,38 @@
 from matplotlib import pyplot as plt
 import random
+import yaml
+import numpy as np
 
 class Boids(object):
-
-    def __init__(self, boids, x_limits=(-500, 1500), y_limits=(-500, 1500)):
+    def __init__(self, boids):
         self.boids = boids
-        self.x_limits = x_limits
-        self.y_limits = y_limits
+        self.config = yaml.load(open("config.yaml"))
         self.fig = plt.figure()
-        self.ax = plt.axes(xlim=self.x_limits, ylim=self.y_limits)
+        self.ax = plt.axes(xlim=self.config['x_plot_limits'], ylim=self.config['y_plot_limits'])
         self.scatter = self.ax.scatter(self.boids[0], self.boids[1])
 
     def update_boids(self):
         xs, ys, xvs, yvs = self.boids
-        move_to_middle_strength = 0.01
-        alert_distance = 100
-        formation_flying_distance = 10000
-        formation_flying_strength = 0.125
+
         # Fly towards the middle
         for i in range(len(xs)):
             for j in range(len(xs)):
-                xvs[i]=xvs[i]+(xs[j]-xs[i])*move_to_middle_strength/len(xs)
-                yvs[i]=yvs[i]+(ys[j]-ys[i])*move_to_middle_strength/len(xs)
-        # Fly away from nearby boids
-                if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < alert_distance:
-                    xvs[i]=xvs[i]+(xs[i]-xs[j])
-                    yvs[i]=yvs[i]+(ys[i]-ys[j])
+                xvs[i] += (xs[j] - xs[i]) * self.config['move_to_middle_strength'] / len(xs)
+                yvs[i] += (ys[j] - ys[i]) * self.config['move_to_middle_strength'] / len(xs)
+                # Fly away from nearby boids
+                if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < self.config['alert_distance']:
+                    xvs[i] += (xs[i] - xs[j])
+                    yvs[i] += (ys[i] - ys[j])
         # Try to match speed with nearby boids
         for i in range(len(xs)):
             for j in range(len(xs)):
-                if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < formation_flying_distance:
-                    xvs[i]=xvs[i]+(xvs[j]-xvs[i])*formation_flying_strength/len(xs)
-                    yvs[i]=yvs[i]+(yvs[j]-yvs[i])*formation_flying_strength/len(xs)
+                if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < self.config['formation_flying_distance']:
+                    xvs[i] += (xvs[j] - xvs[i]) * self.config['formation_flying_strength'] / len(xs)
+                    yvs[i] += (yvs[j] - yvs[i]) * self.config['formation_flying_strength'] / len(xs)
         # Move according to velocities
         for i in range(len(xs)):
-            xs[i]=xs[i]+xvs[i]
-            ys[i]=ys[i]+yvs[i]
+            xs[i] += xvs[i]
+            ys[i] += yvs[i]
         self.boids = xs, ys, xvs, yvs
 
     def animate(self, frame):
--- tests/test_regression.py
+++ tests/test_regression.py
@@ -2,21 +2,13 @@ import boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
-from unittest import TestCase
 
 
-class TestRegression(TestCase):
-
-    def __init__(self, *args, **kwargs):
-
-        super(TestRegression, self).__init__(*args, **kwargs)
-
-    def test_bad_boids_regression(self):
-        regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
-        boid_data = regression_data["before"]
-        test_boids = boids.Boids(boid_data)
-        test_boids.update_boids()
-        for after, before in zip(regression_data["after"], test_boids.boids):
-            for after_value, before_value in zip(after, before):
-                assert_almost_equal(after_value, before_value, delta=0.01)
-
+def test_bad_boids_regression():
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
+    boid_data = regression_data["before"]
+    test_boids = boids.Boids(boid_data)
+    test_boids.update_boids()
+    for after, before in zip(regression_data["after"], test_boids.boids):
+        for after_value, before_value in zip(after, before):
+            assert_almost_equal(after_value, before_value, delta=0.01)
