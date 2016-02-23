53c9061d7987800b98444c9c4304680f1e101ef6
Danial Dervovic <dannydervovic@hotmail.com>
2016-02-23 14:05:24 +0000

    Regression test to work in PyCharm suite

---
+++ tests/test_regression.py
@@ -0,0 +1,21 @@
+from boids import update_boids
+from nose.tools import assert_almost_equal
+import os
+import yaml
+from unittest import TestCase
+
+
+class TestRegression(TestCase):
+
+    def __init__(self, *args, **kwargs):
+
+        super(TestRegression, self).__init__(*args, **kwargs)
+
+    def test_bad_boids_regression(self):
+        regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
+        boid_data = regression_data["before"]
+        update_boids(boid_data)
+        for after, before in zip(regression_data["after"], boid_data):
+            for after_value, before_value in zip(after, before):
+                assert_almost_equal(after_value, before_value, delta=0.01)
+
