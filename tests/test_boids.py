754d8e6bb89c33ffba838d1221729ed4a2c173f4
Danial Dervovic <dannydervovic@hotmail.com>
2016-02-25 13:51:22 +0000

    Tests for numpy refactoring

--- tests/test_boids.py
+++ tests/test_boids.py
@@ -1,8 +1,8 @@
 import boids
-from nose.tools import assert_almost_equal
 import os
 import yaml
 from mock import patch
+from numpy.testing import assert_array_almost_equal
 
 config_filename = 'config.yaml'
 config = yaml.load(open(config_filename))
@@ -16,43 +16,37 @@ def test_boids_fixtures():
     # Regression test
     test_boids.update_boids()
     for after, before in zip(regression_data["reg_after"], test_boids.boids):
-        for after_value, before_value in zip(after, before):
-            assert_almost_equal(after_value, before_value, delta=0.01)
+        assert_array_almost_equal(after, before, 2)
 
     # Test sub_functions
     test_boids.fly_to_middle()
     for after, before in zip(regression_data["fly_to_middle"], test_boids.boids):
-        for after_value, before_value in zip(after, before):
-            assert_almost_equal(after_value, before_value, delta=0.01)
+        assert_array_almost_equal(after, before, 2)
 
     test_boids.fly_away_nearby()
     for after, before in zip(regression_data["fly_away_nearby"], test_boids.boids):
-        for after_value, before_value in zip(after, before):
-            assert_almost_equal(after_value, before_value, delta=0.01)
+        assert_array_almost_equal(after, before, 2)
 
     test_boids.match_speed()
     for after, before in zip(regression_data["match_speed"], test_boids.boids):
-        for after_value, before_value in zip(after, before):
-            assert_almost_equal(after_value, before_value, delta=0.01)
+        assert_array_almost_equal(after, before, 2)
 
     test_boids.move_boids()
     for after, before in zip(regression_data["move_boids"], test_boids.boids):
-        for after_value, before_value in zip(after, before):
-            assert_almost_equal(after_value, before_value, delta=0.01)
+        assert_array_almost_equal(after, before, 2)
 
 
 def test_new_flock():
     num_boids = 20
-    boids_range = (-100, 100)
+    boids_range = [-100, 100]
     test_boids = boids.new_flock(num_boids, boids_range, boids_range, boids_range, boids_range)
 
-    for boid_list in test_boids:
+    for array in test_boids:
         # Check right number of boids
-        assert (len(boid_list) == num_boids)
+        assert cmp(array.shape, (num_boids, 2)) == 0
         # Check boids positions and velocities in range
-        for boid in boid_list:
-            min_boid, max_boid = boids_range
-            assert (min_boid <= boid <= max_boid)
+        in_range = boids_range[0] < array.all() < boids_range[1]
+        assert in_range
 
 
 @patch.object(boids.Boids, 'update_boids')
