eecacbfc1bff6715b7dd7080dd9c1bb23ca2fb93
Danial Dervovic <dannydervovic@hotmail.com>
2016-02-25 22:58:07 +0000

    Simpler restructuring has fixed bug. Was related to matplotlib bug #1656

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="Boids",
     version="0.1.0",
     packages=find_packages(exclude=['*tests']),
-    scripts=['scripts/boids'],
+    scripts=['boids/boids'],
     install_requires=['numpy', 'matplotlib', 'mock', 'pyyaml']
 )
