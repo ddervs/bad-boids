cac9bca5aba851f22834a2ac8ae17b1f0807ca9c
Danial Dervovic <dannydervovic@hotmail.com>
2016-02-26 17:31:39 +0000

    Started report, fixed pep8 in scripts/boids and removed incorrect comment in config file. More setup.py info, changed version number to 1.0.0.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="Boids",
     version="1.0.0",
+    author='Danial Dervovic',
+    author_email='danial.dervovic.11@ucl.ac.uk',
+    url='https://github.com/ddervs/bad-boids',
     packages=find_packages(exclude=['*tests']),
     scripts=['scripts/boids'],
     install_requires=['numpy', 'matplotlib', 'mock', 'pyyaml'],
